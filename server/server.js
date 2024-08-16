445702bd3aef2376b60f8fca4b1d77ebdd381648
schneidsmc <schneidsmc@gmail.com>
2024-08-16 17:57:56 -0500

    Connected Server to Mongo, Post, Get & Delete routes for users

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import routes from './routes/index.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Mongoose
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', routes);
+
+// Example route (endpoint)
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the back-end!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
