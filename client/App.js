0ca2d495e06bdf69702d343226733608a69d0b5b
AMorganH <AMorganHale@gmail.com>
2024-08-21 23:28:24 -0400

    Tryin sumthin
    trying to get expo to work - will move the two json files back

--- client/App.js
+++ client/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MyCloset App</Text>
+      <Text>Fullstack Closet App for real</Text>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
