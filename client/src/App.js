c7dd97ec956fb344081c3b8c5f36167a04983f21
AMorganH <AMorganHale@gmail.com>
2024-08-22 23:52:05 -0400

    React/bootstrap front end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import HelloWorld from './Components/HelloWorld';
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
