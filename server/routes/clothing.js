c084a0913d92b35e48757265b9ca5e4c45846607
schneidsmc <schneidsmc@gmail.com>
2024-09-22 12:03:28 -0500

    more comments

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -22,7 +22,7 @@ const cloudinaryUpload = (buffer) => {
     streamifier.createReadStream(buffer).pipe(stream);
   });
 };
-
+// Add Item
 router.post(
   "/",
   authenticateToken,
@@ -59,7 +59,7 @@ router.post(
     }
   },
 );
-
+// CLOSET PAGE CLOTHES
 router.get("/clothing", authenticateToken, async (req, res) => {
   try {
     const clothes = await Clothing.find({ user: req.user.id });
