a68f5da0b7b7ad13717a22d47dafed846b21e30c
AMorganH <AMorganHale@gmail.com>
2024-09-22 23:03:11 -0400

    added some bootstrap to closet

--- client/src/Pages/ClosetPage.js
+++ client/src/Pages/ClosetPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import axios from "axios";
 
 const ClosetPage = () => {
@@ -46,7 +48,11 @@ const ClosetPage = () => {
         {/* {userName}'s Closet */}
         CLOSET
       </h1>
-
+      <div className="text-center">
+        <Link to="/camera">
+          <Button variant="primary" className="mb-3">Add Item</Button>
+        </Link>
+      </div>
       <ul>
         {Array.isArray(clothingItems) && clothingItems.length > 0 ? (
           clothingItems.map((item) => (
@@ -55,15 +61,26 @@ const ClosetPage = () => {
               <p>Brand: {item.brand}</p>
               <p>Size: {item.size}</p>
               <p>Color: {item.color}</p>
-              <img
-                src={item.image}
-                alt={`${item.category} ${item.brand} ${item.color}`}
-                width="100"
+              <div className="card"></div>
+                <img
+                  src={item.image}
+                  alt={`${item.category} ${item.brand} ${item.color}`}
+                  className="card-img-top"
+                  style={{ height: '200px', objectFit: 'cover' }}
               />
+              <div className="card-body"></div>
+              <p className="card-text">
+                    <strong>Category:</strong> {item.category}<br />
+                    <strong>Size:</strong> {item.size}<br />
+                    <strong>Color:</strong> {item.color}
+                    <strong>Brand:</strong> {item.color}
+              </p>
             </li>
           ))
         ) : (
-          <p>YOU GOT NO CLOTHES</p>
+          <div className="col-12">
+            <p className="text-center">YOU GOT NO CLOTHES</p>
+          </div>
         )}
       </ul>
     </div>
