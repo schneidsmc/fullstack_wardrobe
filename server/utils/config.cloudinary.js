57c59fb7614b936e37dad688b4d617edd0344ffb
schneidsmc <schneidsmc@gmail.com>
2024-10-12 18:49:40 -0500

    gitignore and cloudinary updates

--- server/utils/config.cloudinary.js
+++ server/utils/config.cloudinary.js
@@ -5,6 +5,7 @@ dotenv.config({path: '../.env'});
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  secure: true,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
