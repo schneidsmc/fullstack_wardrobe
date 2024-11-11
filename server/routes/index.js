282f7cb8ae0d3ad521fbe1bec5348c5e5ded66d9
schneidsmc <schneidsmc@gmail.com>
2024-11-11 13:13:03 -0600

    random route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./users.js";
 import clothingRoutes from "./clothing.js";
+import randomOutfit from "./random.js"
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/upload", clothingRoutes);
+router.use("/randomOutfit", randomOutfit)
 
 export default router;
---
+++ server/routes/random.js
@@ -0,0 +1,26 @@
+import express from "express";
+import Clothing from "../models/Clothing.js";
+import authenticateToken from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Generates random outfits
+router.get("/randomOutfit", authenticateToken, async (req, req) =>{
+    try {
+        const userId = req.user.id;
+        const categories = ["tops", "bottoms", "accessories", "shoes"];
+        const randomItems = {};
+
+        for (const category of categories) {
+            randomItems[category] = await Clothing.aggregate([
+                { $match : {user: userId, category}},
+                { $sample: {size: 1} }
+            ])
+        }
+
+        res.status(200).json(randomItems);
+    } catch (error) {
+        console.error("Can't fetch your fetch outfit", error)
+        res.status(500).json({error: "Fetch outfit is so not fetched"})
+    }
+})
