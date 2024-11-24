fe9853444025ae1b579b9965fee2761ee9b67acf
schneidsmc <schneidsmc@gmail.com>
2024-11-24 13:50:40 -0600

    outfit model created and psuedo coded the outfit routes needed

---
+++ server/routes/outfits.js
@@ -0,0 +1,14 @@
+import express from "express"
+import authenticateToken from "../middleware/authMiddleware.js"
+import Outfit from "../models/Outfit.js"
+
+const router = express.Router();
+
+// Create new outfit
+
+// Get all outfits for the LOGGED IN user
+
+// View single outfit by ID
+
+// Delete outfit
+
